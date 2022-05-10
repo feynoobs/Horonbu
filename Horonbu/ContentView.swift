b7972196206a47ba137d4ea6ed3df93d5990e434
M.Anaguchi <p.w.feeniej@gmail.com>
2022-05-10 14:31:01 +0900

    1st

---
+++ Horonbu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Horonbu
+//
+//  Created by 穴口昌宏 on 2022/05/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Horonbu/HoronbuApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoronbuApp.swift
+//  Horonbu
+//
+//  Created by 穴口昌宏 on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct HoronbuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
